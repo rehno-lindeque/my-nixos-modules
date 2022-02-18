125a4527028240db4761ae173a9c1c66369cc160
Rehno Lindeque <errantkid@gmail.com>
2022-02-18 12:08:28 -0500

    TODO for making a basler group

--- modules/hardware/basler-camera/default.nix
+++ modules/hardware/basler-camera/default.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
 # See also
+# * https://www.baslerweb.com/en/support/downloads/software-downloads/pylon-5-0-9-linux-x86-64-bit/
+# * https://www.baslerweb.com/fp-1496750153/media/downloads/software/pylon_software/pylon-5.0.9.10389-x86_64.tar.gz
 # * https://github.com/AravisProject/aravis/blob/master/aravis.rules
 
 with lib;
@@ -24,8 +26,18 @@ in
         default = false;
         description = ''
           Enable basler udev rules.
+          By default, a basler group is created for this purpose.
         '';
       };
+      # TODO
+      # group = mkOption {
+      #   type = types.str;
+      #   default = "basler";
+      #   example = "users";
+      #   description = ''
+      #     Grant access to basler devices to users in this group.
+      #   '';
+      # };
     };
   };
 
