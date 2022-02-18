ad4366d2d84b32323e8f11e27def7d99f8e1dc17
Rehno Lindeque <errantkid@gmail.com>
2022-02-18 12:07:21 -0500

    WIP: teensy
    Not sure whether this is helpful yet

--- modules/hardware/teensy/default.nix
+++ modules/hardware/teensy/default.nix
@@ -8,7 +8,7 @@ let
     # Enable user access to teensy USB development board
     name = "teensy";
     text = ''
-      ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+      ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1", ENV{ID_MM_PORT_IGNORE}="1"
       ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
       SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
       KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
@@ -24,8 +24,18 @@ in
         default = false;
         description = ''
           Enable teensy udev rules.
+          By default, a teensy group is created for this purpose.
         '';
       };
+      # TODO
+      # group = mkOption {
+      #   type = types.str;
+      #   default = "teensy";
+      #   example = "users";
+      #   description = ''
+      #     Grant access to teensy devices to users in this group.
+      #   '';
+      # };
     };
   };
 
